5437089608da066fba8f580b70fa288a0c25a6e5
MFXZ <109869000+MFXZ@users.noreply.github.com>
2022-08-06 18:34:41 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 let fs = require('fs')
 
-global.owner = ['6281351047727'] // Put your number here
+global.owner = ['628981614707'] // Put your number here
 global.mods = [] // Want some help?
 global.prems = JSON.parse(fs.readFileSync('./src/premium.json'))
 global.APIs = { // API Prefix
@@ -28,22 +28,22 @@ global.APIKeys = { // APIKey Here
 
 // Sticker WM
 global.stiker_wait = 'Stiker sedang dibuat'
-global.packname = 'ShiraoriBOT Multi device'
-global.author = 'Ilmanhdyt'
-global.namabot = 'ShiraoriBOT'
+global.packname = 'HAYOYO'
+global.author = 'BY MFC BOT'
+global.namabot = 'MfcBOT'
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
-global.wm = '© ShiraoriBOT-Md • Ilmanhdyt'
+global.wm = '© MfcBOT-Md • HAYOYO'
 global.media = 'https://telegra.ph/file/5d45a3d1e12726d98c5e4.jpg'
 
 global.wait = '_*tunggu sedang di proses...*_'
 global.eror = '_*Server Error*_'
 
 global.dtu = 'ɪɴꜱᴛᴀɢʀᴀᴍ'
-global.urlnya = "https://www.instagram.com/ilmanhdyt_"
+global.urlnya = "-"
 
 //============= callButtons =============//
 global.dtc = 'ᴄᴀʟʟ ᴏᴡɴᴇʀ'
-global.phn = '+62 813-5104-7727'
+global.phn = '+62 898-1614-707'
 
 //============= Games ================//
 global.benar = '_*Benar✅*_'
